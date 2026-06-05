a2ac5700963f1db2f308b528541c817d78c3d508
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-05 16:16:25 -0300

    feat: adiciona migrations

---
+++ database/migrations/2026_06_05_183424_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('category_id')
+                ->constrained()
+                ->cascadeOnDelete();
+
+            $table->string('name', 255);
+            $table->decimal('price', 10, 2);
+            $table->string('description', 255);
+            $table->boolean('is_available')->default(true);
+
+            $table->unique(['category_id', 'name']);
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
