38ecd62df082a75c6d75d8cb8600a8c78e321cbf
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-06 16:04:06 -0300

    feat: adiciona DTO para cadastro de um restaurante

---
+++ app/DTOs/StoreRestaurantDTO.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\DTOs;
+
+use App\Http\Requests\StoreRestaurantRequest;
+
+class StoreRestaurantDTO
+{
+    public function __construct(
+        public readonly string $name,
+        public readonly string $address
+    ) {}
+
+    public static function fromRequest(StoreRestaurantRequest $request): self
+    {
+        return new self(
+            name: $request->string('name'),
+            address: $request->string('address')
+        );
+    }
+}
