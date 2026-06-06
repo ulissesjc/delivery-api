943b94e99d0490598e084b41e1d44b356c868dde
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-06 16:18:06 -0300

    feat: adiciona request de edição de um restaurante

---
+++ app/Http/Requests/UpdateRestaurantRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateRestaurantRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'string', 'min:3', 'max:255'],
+            'address' => ['sometimes', 'string', 'min:3', 'max:255'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.string' => 'O nome deve ser um texto válido',
+            'name.min' => 'O nome deve ter no mínimo :min caracteres',
+            'name.max' => 'O nome deve ter no máximo :max caracteres',
+
+            'address.string' => 'O endereço deve ser um texto válido',
+            'address.min' => 'O endereço deve ter no mínimo :min caracteres',
+            'address.max' => 'O endereço deve ter no máximo :max caracteres',
+        ];
+    }
+}
