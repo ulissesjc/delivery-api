7e1fb1aaedb2de9bd74115bc5f47a2f4779a9611
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-08 17:45:22 -0300

    feat: adiciona tratamento de exceções

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,8 +1,12 @@
 <?php
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -15,5 +19,30 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        $exceptions->shouldRenderJsonWhen(function (Request $request) {
+            return $request->is('api/*') || $request->wantsJson();
+        });
+
+        $exceptions->render(function (Throwable $e) {
+
+            if ($e instanceof ValidationException) {
+                return null;
+            }
+
+            if ($e instanceof AuthenticationException) {
+                return response()->json([
+                    'message' => 'Não autenticado',
+                ], 401);
+            }
+
+            if ($e instanceof NotFoundHttpException) {
+                return response()->json([
+                    'message' => 'Recurso não encontrado',
+                ], 404);
+            }
+
+            return response()->json([
+                'message' => 'Erro interno no servidor',
+            ], 500);
+        });
     })->create();
