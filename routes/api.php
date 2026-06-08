6e976bcf43f71250582d0786a37a04b756a123b8
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-08 17:15:38 -0300

    feat: adiciona rota das operações de um restaurante

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return response()->json(['message' => 'OK'], 200);
 });
+
+Route::apiResource('restaurants', RestaurantController::class);
