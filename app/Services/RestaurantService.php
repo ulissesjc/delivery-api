6932bb33b334e040bf21776f1c633badfec4daf3
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-08 17:14:02 -0300

    feat: adiciona service do módulo de restaurantes

---
+++ app/Services/RestaurantService.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Services;
+
+use App\DTOs\CreateRestaurantDTO;
+use App\DTOs\UpdateRestaurantDTO;
+use App\Models\Restaurant;
+use App\Repositories\Contracts\RestaurantRepositoryInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class RestaurantService
+{
+    public function __construct(
+        protected RestaurantRepositoryInterface $repository
+    ) {}
+
+    public function getAll(int $perPage): LengthAwarePaginator
+    {
+        return $this->repository->getAll($perPage);
+    }
+
+    public function findOne(string $id): Restaurant
+    {
+        return $this->repository->findOne($id);
+    }
+
+    public function new(CreateRestaurantDTO $dto): Restaurant
+    {
+        return $this->repository->new($dto);
+    }
+
+    public function update(string $id, UpdateRestaurantDTO $dto): Restaurant
+    {
+        return $this->repository->update($id, $dto);
+    }
+
+    public function delete(string $id): void
+    {
+        $this->repository->delete($id);
+    }
+}
