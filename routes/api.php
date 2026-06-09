945aaa2121175cb8bfca6d64120ffe5c50b23e5d
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-09 17:20:22 -0300

    feat: adiciona rota das operações de um produto

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,14 @@
 <?php
 
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return response()->json(['message' => 'OK'], 200);
-});
 
 Route::apiResource('restaurants', RestaurantController::class);
 
 Route::apiResource('restaurants.categories', CategoryController::class)->only('index', 'store');
 Route::apiResource('categories', CategoryController::class)->only('show', 'update', 'destroy');
+
+Route::apiResource('categories.products', ProductController::class)->only('index', 'store');
+Route::apiResource('products', ProductController::class)->only('show', 'update', 'destroy');
