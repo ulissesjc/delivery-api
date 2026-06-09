145e1a026239af5a2e7046cc69ec73990677bb64
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-09 17:30:39 -0300

    feat: adiciona request de edição de um produto

---
+++ app/Http/Requests/UpdateProductRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProductRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['sometimes', 'string', 'min:5', 'max:255'],
+            'price' => ['sometimes', 'numeric', 'gt:0'],
+            'description' => ['sometimes', 'string', 'min:5', 'max:255'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'O nome é obrigatório',
+            'name.string' => 'O nome deve ser um texto válido',
+            'name.min' => 'O nome deve ter no mínimo :min caracteres',
+            'name.max' => 'O nome deve ter no máximo :max caracteres',
+
+            'price.required' => 'O preço é obrigatório',
+            'price.numeric' => 'O preço deve ser um valor numérico',
+            'price.gt' => 'O preço deve ser maior que 0',
+
+            'description.string' => 'A descrição deve ser um texto válido',
+            'description.min' => 'A descrição deve ter no mínimo :min caracteres',
+            'description.max' => 'A descrição deve ter no máximo :max caracteres',
+        ];
+    }
+}
