d7dbe81199f2e3c2f29b50fb8010eb3087ec7e90
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-10 16:52:44 -0300

    feat: adiciona request para edição do status de um pedido

---
+++ app/Http/Requests/UpdateOrderStatus.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Enums\OrderStatus;
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateOrderStatus extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'status' => ['required', Rule::enum(OrderStatus::class)],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'status.required' => 'O status é obrigatório',
+            'status.enum' => 'O status informado é inválido',
+        ];
+    }
+}
