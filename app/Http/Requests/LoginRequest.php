a6b0b899b36b6437f72ee979fa5d95ea854b70c7
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-10 17:48:26 -0300

    feat: adiciona request de login

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'email'],
+            'password' => ['required', 'string'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'email.required' => 'O e-mail é obrigatório',
+            'email.email' => 'É necessário informar um e-mail válido',
+
+            'password.required' => 'A senha é obrigatória',
+            'password.string' => 'A senha deve ser um texto válido',
+        ];
+    }
+}
