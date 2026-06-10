5ec1854c81702636036dd4b198f000eb4baf2464
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-10 17:48:06 -0300

    feat: protege endpoints da API com auth:sanctum e adiciona endpoints referentes à autenticação

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
+    use HasApiTokens;
+
     /**
      * The attributes that are mass assignable.
      *
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\RestaurantController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('restaurants', RestaurantController::class);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login'])->name('login');
 
-Route::apiResource('restaurants.categories', CategoryController::class)->only('index', 'store');
-Route::apiResource('categories', CategoryController::class)->only('show', 'update', 'destroy');
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
 
-Route::apiResource('categories.products', ProductController::class)->only('index', 'store');
-Route::apiResource('products', ProductController::class)->only('show', 'update', 'destroy');
+    Route::apiResource('restaurants', RestaurantController::class);
 
-Route::apiResource('restaurants.orders', OrderController::class)->only(['index', 'store']);
-Route::apiResource('orders', OrderController::class)->only(['show']);
-Route::patch('/orders/{order}/status', [OrderController::class, 'updateStatus']);
+    Route::apiResource('restaurants.categories', CategoryController::class)->only('index', 'store');
+    Route::apiResource('categories', CategoryController::class)->only('show', 'update', 'destroy');
+
+    Route::apiResource('categories.products', ProductController::class)->only('index', 'store');
+    Route::apiResource('products', ProductController::class)->only('show', 'update', 'destroy');
+
+    Route::apiResource('restaurants.orders', OrderController::class)->only(['index', 'store']);
+    Route::apiResource('orders', OrderController::class)->only(['show']);
+    Route::patch('/orders/{order}/status', [OrderController::class, 'updateStatus']);
+});
