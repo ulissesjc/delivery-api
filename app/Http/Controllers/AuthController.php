2a5abbc35e73af02863272d343e1c9a370ce7495
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-11 14:48:45 -0300

    fix: corrige o status de resposta de login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,7 +32,7 @@ class AuthController extends Controller
 
         return response()->json([
             'token' => $user->createToken($user->name)->plainTextToken,
-        ], 201);
+        ], 200);
     }
 
     public function logout(Request $request): JsonResponse
