be3c7e8e7fb077faa955f647e1cd209cbbe23c62
Ulisses Cavalcante <ulisses.c.2811@gmail.com>
2026-06-16 11:49:43 -0300

    feat: adiciona verificação de existência de uma categoria na criação de um produto

--- app/Services/ProductService.php
+++ app/Services/ProductService.php
@@ -6,41 +6,45 @@ use App\DTOs\CreateProductDTO;
 use App\DTOs\UpdateProductDTO;
 use App\Exceptions\BusinessException;
 use App\Models\Product;
+use App\Repositories\Contracts\CategoryRepositoryInterface;
 use App\Repositories\Contracts\ProductRepositoryInterface;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 class ProductService
 {
     public function __construct(
-        protected ProductRepositoryInterface $repository
+        protected ProductRepositoryInterface $productRepository,
+        protected CategoryRepositoryInterface $categoryRepository
     ) {}
 
     public function getAll(string $categoryId, int $perPage): LengthAwarePaginator
     {
-        return $this->repository->getAll($categoryId, $perPage);
+        return $this->productRepository->getAll($categoryId, $perPage);
     }
 
     public function findOne(string $id): Product
     {
-        return $this->repository->findOne($id);
+        return $this->productRepository->findOne($id);
     }
 
     public function new(CreateProductDTO $dto): Product
     {
-        if ($this->repository->alreadyExistsInCategory($dto->name, $dto->category_id)) {
+        $this->categoryRepository->findOne($dto->category_id);
+
+        if ($this->productRepository->alreadyExistsInCategory($dto->name, $dto->category_id)) {
             throw new BusinessException('Este produto já está cadastrado nesta categoria');
         }
 
-        return $this->repository->new($dto);
+        return $this->productRepository->new($dto);
     }
 
     public function update(string $id, UpdateProductDTO $dto): Product
     {
-        return $this->repository->update($id, $dto);
+        return $this->productRepository->update($id, $dto);
     }
 
     public function delete(string $id): void
     {
-        $this->repository->delete($id);
+        $this->productRepository->delete($id);
     }
 }
